4725cb6f0022a42c1602463022f014fb4bdd2b8b
Martin Tomasi <martin.tomasi@gmail.com>
2021-02-26 15:35:39 +0100

    node backend begone, add java placeholder project

---
+++ backend/src/main/java/xyz/tomasi/videosync/VideoSyncApplication.java
@@ -0,0 +1,13 @@
+package xyz.tomasi.videosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoSyncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/xyz/tomasi/videosync/VideoSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tomasi.videosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
