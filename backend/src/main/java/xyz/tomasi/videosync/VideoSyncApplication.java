15518548ff2fd5b264fd8b191843c14cdd5c1248
Martin Tomasi <martin.tomasi@gmail.com>
2021-02-26 16:39:54 +0100

    slight cleanup

--- backend/src/main/java/xyz/tomasi/videosync/VideoSyncApplication.java
+++ backend/src/main/java/xyz/tomasi/videosync/VideoSyncApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VideoSyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideoSyncApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(VideoSyncApplication.class, args);
+  }
 }
