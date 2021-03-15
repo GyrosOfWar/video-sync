980359408bd2de779c5222e97005e196250204ac
Martin Tomasi <martin.tomasi@gmail.com>
2021-03-15 18:37:23 +0100

    added test (wip)

---
+++ backend/src/test/java/xyz/tomasi/videosync/controller/RoomWebSocketHandlerTest.java
@@ -0,0 +1,38 @@
+package xyz.tomasi.videosync.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.socket.HandshakeInfo;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class RoomWebSocketHandlerTest {
+
+  @Autowired
+  private RoomWebSocketHandler handler;
+
+  @Test
+  void testConnect() {
+    var url = "http://localhost:8080/ws/room/604942312807237b0e33ca8c";
+    var info = mock(HandshakeInfo.class);
+    when(info.getUri()).thenReturn(URI.create(url));
+    var session = mock(WebSocketSession.class);
+    when(session.getHandshakeInfo()).thenReturn(info);
+
+//    when(session.receive()).thenReturn(
+//
+//    );
+
+    handler.handle(session).block();
+  }
+}
